81ff042101967308f095f2653351874a08f82baf
pixel <pixelisprocrastinating15@gmail.com>
2025-09-11 16:14:34 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -54,6 +54,10 @@ export default {
                             <div class="type-title-sm">version</div>
                             <p>{{ level.version || '2.2' }}</p>
                         </li>
+                        <li>
+                            <div class="type-title-sm">FPS</div>
+                            <p>{{ level.fps || 'Any' }}</p>
+                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
