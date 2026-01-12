a64eeab14ddf64eedf4681a292d6a168f519a198
finni1505 <lieverscheidtfinn@gmail.com>
2026-01-12 10:01:59 +0100

    Update scoring formula in score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-25*Math.pow(rank-1, 0.47) + 250) *
+    let score = (-25*Math.pow(rank-1, 0.45) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
